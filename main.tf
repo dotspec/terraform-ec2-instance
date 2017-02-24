20abcf3e3762d25763ae7652d1c9b0cc2f9b01f4
darreng <darreng@spaceflightindustries.com>
2017-02-24 13:44:20 -0800

    adding a user-data var

--- main.tf
+++ main.tf
@@ -9,11 +9,12 @@ variable "ec2_security_groups" { type = "list" }
 variable "ec2_subnet_id" { type = "string" }
 variable "ec2_source_dest_check" { default = true }
 variable "ec2_termination_protection" { default = true }
+variable "ec2_user_data" { type = "string" }
 
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.aws_region}"
+  region     = "${var.aws_region}"
 }
 
 resource "aws_instance" "ec2_instance" {
@@ -25,4 +26,5 @@ resource "aws_instance" "ec2_instance" {
   subnet_id               = "${var.ec2_subnet_id}"
   source_dest_check       = "${var.ec2_source_dest_check}"
   disable_api_termination = "${var.ec2_termination_protection}"
+  user-data               = "${var.ec2_user_data}"
 }
